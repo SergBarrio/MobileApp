1d8eb1e988aee5482e2197a9def1009efad1b6c4
SergBarrio <serg.barrio@gmail.com>
2013-04-23 13:42:56 -0500

    Updated for things on internet
    These are the files as they look on the current website,
    http://people.tamu.edu/~serg_barrio/Fridge_Raider/

--- phonegap/com.simonmacdonald.scan.BarcodeScanner/assets/www/js/utils.js
+++ phonegap/com.simonmacdonald.scan.BarcodeScanner/assets/www/js/utils.js
@@ -1,5 +1,26 @@
 function formcenter() {
-			var center = $("#formcenter");
-			center.height( window.innerHeight - 20 );
-			center.width( window.innerWidth );
+	var center = $("#formcenter");
+	center.height( window.innerHeight - 20 );
+	center.width( window.innerWidth );
+}
+
+function logIn() {
+	var username = document.getElementById("username");
+	var password = document.getElementById("password");
+	var ROOT = "https://hungrynorse.appspot.com/_ah/api";
+	gapi.client.load('hungrynorse', 'v1', function() {
+			console.log("About to Send Data");
+			gapi.client.hungrynorse.users.login({"email":username.value, "password":password.value}).execute(logInResponse);
+		}, ROOT);
+}
+
+function logInResponse(data){
+	console.log(data);
+	
+	var curdate = new Date();
+	if ( (curdate > Date.parse(data.dateJoined)) && data.dateJoined !== undefined){
+		window.location.href = "main.html";
+	} else {
+		alert("Not a valid email or password!");
+	}
 }
